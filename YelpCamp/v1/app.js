4caa62b695f2d68ef1377959c4f52a33198d2a0f
Jonah Fidel <jonahfidel@gmail.com>
2019-05-14 11:50:31 -0400

    Moved landing into views directory.

--- YelpCamp/v1/app.js
+++ YelpCamp/v1/app.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
 
+// set render engine to ejs to allow render routing to ejs template files
+app.set("view engine", "ejs");
+
 app.get('/', function(req, res){
-	res.send("This will be the landing page soon");
+	res.render("landing");
 });
 
+// alternate port setup for hosted
+// process.env.PORT, process.env.IP
 app.listen(8000, function(){
 	console.log("The YelpCamp server has started.")
 });
