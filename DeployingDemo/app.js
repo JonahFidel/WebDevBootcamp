a2e56b51787f457f64bc4c23280cd23e26736eac
Jonah Fidel <jonahfidel@gmail.com>
2019-05-23 17:04:07 -0400

    Deployment demo.

---
+++ DeployingDemo/app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res)=>{
+	res.render("home");
+});
+
+app.get("/about", (req, res)=>{
+	res.render("about");
+});
+
+app.listen(process.env.PORT, function(err){
+	console.log("Server started!");
+});
